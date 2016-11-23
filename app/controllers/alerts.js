3c25d42304ff77de7f4da07efcf066d07159fed4
Bacquet <nabacquet@gmail.com>
2016-11-23 14:03:47 +0100

    fix(alerts) : add userid field in static response of getOne and getAll

--- app/controllers/alerts.js
+++ app/controllers/alerts.js
@@ -8,6 +8,7 @@ module.exports = function(router, Users) {
   function getAll(req, res) {
     return res.json([{
       id: 1,
+      userId : "5807394d416656001d4012e7",
       state: 'Perdu',
       photo: 'http://www.apagi.fr/media/filer_public/37/85/3785774d-1d65-4a7c-8f44-e6175f92a603/jumper-chien-male-yorkshire-noir-et-feu-1.jpg',
       date: '10-08-2016',
@@ -27,6 +28,7 @@ module.exports = function(router, Users) {
     // res.json(Users.findAsync({_id: req.params.id}));
     return res.json({
       id: req.params.id,
+      userId : "5807394d416656001d4012e7",
       state: 'Perdu',
       photo: 'http://www.apagi.fr/media/filer_public/37/85/3785774d-1d65-4a7c-8f44-e6175f92a603/jumper-chien-male-yorkshire-noir-et-feu-1.jpg',
       date: '10-08-2016',
